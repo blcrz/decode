82639b4e7271d68cd40163b9b88f78f3e3568e9e
Bárbara Cruz <ba.luizacruz@gmail.com>
2021-08-29 15:08:03 -0300

    adicionadas codificação e decodificação

--- js/decodificar.js
+++ js/decodificar.js
@@ -1,11 +1,10 @@
 var texto = document.querySelector('#campo');
-var valorTexto = texto.value
 
 var select = document.querySelector('#select')
 
 var incremento = document.querySelector('#incremento')
 
-var radioCode = document.querySelector('#code')
+var radioCode = document.querySelector('#cod')
 
 var radioDecode = document.querySelector('#decode')
 
@@ -19,15 +18,91 @@ select.addEventListener('change',function(){
     }
 })
 
-radioCode.addEventListener(click, function(){
-    var result = document.querySelector('#resultado');
-    if(select.value == 'cifra'){
-        var arr = [];
-        var code = texto.value.split(' ')[0];
+radioCode.addEventListener('click', function(){
+    botao.value = 'Code'
+})
 
-        for(var i = 0; i < code.length; i++) {
-            var num = code[i].charCodeAt(0).toUpperCase();
-            var numFinal = numero+parseInt(incremento.value)
-        }
+radioDecode.addEventListener('click', function(){
+    botao.value = 'Decode'
+})
+
+botao.addEventListener('click', function(e){
+    e.preventDefault();
+    let result = document.querySelector('#resultado');
+    let valorTexto = texto.value;
+    if(radioCode.checked){
+        codificar(result, valorTexto)
+    }else{
+        decodificar(result, valorTexto)
     }
 })
+
+function codificar(result, valorTexto){
+    if(select.value == 'cifra'){
+        let arr = [];
+
+        for(var i = 0; i < valorTexto.length; i++){
+            if(valorTexto[i].charCodeAt() >= 65 && valorTexto[i].charCodeAt() <= 90){
+                arr.push(String.fromCharCode(
+                    ((valorTexto[i].charCodeAt() - 65 + parseInt(incremento.value))%26) + 65
+                ));
+            }else if(valorTexto[i].charCodeAt() >= 97 && valorTexto[i].charCodeAt() <= 122){
+                arr.push(String.fromCharCode(
+                    ((valorTexto[i].charCodeAt() - 97 + parseInt(incremento.value))%26) + 97
+                ));
+            }
+            else{
+                arr.push(valorTexto[i])
+            }
+        }
+     result.value = arr.join('');
+    }else if(select.value == 'base'){
+        let base64 = btoa(texto.value);
+        result.value = base64;
+    }
+}
+
+function decodificar(result, valorTexto){
+    if(select.value === 'cifra'){
+        let arr = [];
+        let incrementoValor = parseInt(incremento.value)
+        for(var i = 0; i < valorTexto.length; i++){
+            if(valorTexto[i].charCodeAt() >= 65 && valorTexto[i].charCodeAt() <= 90){
+                console.log(valorTexto[i].charCodeAt() - 65 - incrementoValor)
+                if(valorTexto[i].charCodeAt() - 65 - incrementoValor < 0) {
+                    arr.push(
+                        String.fromCharCode(
+                            ((valorTexto[i].charCodeAt() - 65 - incrementoValor + 26) % 26)+65
+                        )
+                    )
+                }else{
+                    arr.push(
+                        String.fromCharCode(
+                            ((valorTexto[i].charCodeAt() - 65 - incrementoValor) % 26) + 65
+                        )
+                    )
+                }
+            }else if(valorTexto[i].charCodeAt() >= 97 && valorTexto[i].charCodeAt() <= 122){
+                if(valorTexto[i].charCodeAt() - 97 - incrementoValor < 0) {
+                    arr.push(
+                        String.fromCharCode(
+                            ((valorTexto[i].charCodeAt() - 97 - incrementoValor + 26) % 26)+97
+                        )
+                    )
+                }else{
+                    arr.push(
+                        String.fromCharCode(
+                            ((valorTexto[i].charCodeAt() - 97 - incrementoValor) % 26) + 97
+                        )
+                    )
+                }
+            }
+            else{
+                arr.push(valorTexto[i])
+            }
+        }
+        result.value = arr.join('');
+    }else{
+        result.value = atob(valorTexto);
+    }
+}
